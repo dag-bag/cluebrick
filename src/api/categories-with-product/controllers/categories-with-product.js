b65bdd6aecded1dcf63b4170722d827c1b746898
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-06-01 14:28:19 -0700

    New Update

---
+++ src/api/categories-with-product/controllers/categories-with-product.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * categories-with-product controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::categories-with-product.categories-with-product');
---
+++ src/api/categories-with-product/services/categories-with-product.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * categories-with-product service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::categories-with-product.categories-with-product');
