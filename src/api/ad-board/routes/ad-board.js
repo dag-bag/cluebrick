3bdd71a9a01212c7004d71619f9bcf068eee8935
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-06-02 03:09:56 -0700

    Update ad board

---
+++ src/api/ad-board/routes/ad-board.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ad-board router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ad-board.ad-board');
---
+++ src/api/ad-board/services/ad-board.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ad-board service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ad-board.ad-board');
