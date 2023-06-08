35332b29813d3874b22a0a8e88b36145ed8c7c2a
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-06-08 07:38:01 -0700

    Second Production Update

---
+++ src/api/fulfillment-provider/routes/fulfillment-provider.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * fulfillment-provider router.
+ */
+
+const { createCoreRouter } = require("@strapi/strapi").factories;
+
+module.exports = createCoreRouter(
+  "api::fulfillment-provider.fulfillment-provider"
+);
