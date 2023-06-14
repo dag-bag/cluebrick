191a8ef014e323d88aa16534eda44c009b299ef4
dag-bag <virenderkumar23435@gmail.com>
2023-06-14 15:23:41 -0700

    Email Update

--- config/server.js
+++ config/server.js
@@ -1,6 +1,14 @@
+const forgotPasswordTemplate = require("./email-template/forgot-password");
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    forgotPassword: {
+      from: "support@mywebsite.fr",
+      replyTo: "support@mywebsite.fr",
+      emailTemplate: forgotPasswordTemplate,
+    },
+  },
   app: {
     keys: env.array("APP_KEYS"),
   },
